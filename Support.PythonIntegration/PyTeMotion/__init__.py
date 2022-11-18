0371ffd6f8451a6122b01651ff078a32adcedd52
SimonStrassen97 <simon.strassen@outlook.com>
2022-11-18 14:15:51 +0100

    Creation

---
+++ Support.PythonIntegration/PyTeMotion/__init__.py
@@ -0,0 +1,54 @@
+import os
+import sys
+sys.path.append(os.path.realpath('..\Support.AxisWrappers\\bin\Debug'))
+
+import clr
+clr.AddReference("Support.AxisWrappers")
+
+from typing import SupportsFloat as Numeric
+
+from Support.AxisWrappers import AxisWrapper
+from Support.AxisWrappers import ConfigurationHelper
+
+
+
+
+
+
+class Axis:
+
+    def __init__(self, modul: str, axis: str, configfile: str) -> None:
+        self.axisAbstraction = AxisWrapper(ConfigurationHelper.GetAxis(modul, axis, configfile), modul+"."+axis+"_log")
+        pass
+
+    def StartTeControl(self) -> None:
+        self.axisAbstraction.StartTeControl()
+        pass
+
+    def Initialize(self) -> None:
+        self.axisAbstraction.Initialize()
+        pass
+
+    def MoveTo(self, target: Numeric) -> None:
+        self.axisAbstraction.MoveTo(float(target))
+        pass
+
+    def MoveFor(self, distance: Numeric) -> None:
+        self.axisAbstraction.MoveFor(float(distance))
+        pass
+
+    def WriteLog(self) -> None:
+        self.axisAbstraction.WriteLog()
+        pass
+
+
+if __name__ == "__main__":
+
+    CGA_y = Axis("CGA", "y",  '..\..\..\..\ConfigurationData\Fluent_CGA_Config.xml')
+
+    CGA_y.StartTeControl()
+    CGA_y.Initialize()
+
+    for x in [111.0, 222.0, 333.0, 222.0, 111.0, 123.0]:
+        CGA_y.MoveTo(x)
+        pass
---
+++ Support.PythonIntegration/example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 16:00:20 2022
+
+@author: MA012002
+"""
+import os
+import sys
+from PyTeMotion import Axis
+
+
+CGA_y = Axis("CGA", "y", os.path.realpath('..\ConfigurationData\RGA\Fluent_CGA_Config.xml'))
+CGA_z = Axis("CGA", "zshort", os.path.realpath('..\ConfigurationData\RGA\Fluent_CGA_Config.xml'))
+
+CGA_y.StartTeControl()
+CGA_y.Initialize()
+# CGA_z.Initialize()
+
+for x in [110, 222.2, 333, 222, 111.0, 123.0]:
+    CGA_y.MoveTo(x)
+    pass
